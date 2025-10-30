743e098ceb9c196b728066e0009b88a87b11b7c2
sunluyu <13023509833@163.com>
2025-10-30 20:46:17 +0800

    refactor(vuepress): 重构主题配置并更新文档结构
    - 替换头像图片格式从png到jpg
    - 删除旧文章和模板文件
    - 更新主题配置，包括标题、作者信息和导航栏
    - 清理公告栏配置
    - 调整文档格式和样式

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,17 +1,17 @@
 import { defineUserConfig } from "vuepress";
 import recoTheme from "vuepress-theme-reco";
-import { viteBundler } from '@vuepress/bundler-vite'
-import { webpackBundler } from '@vuepress/bundler-webpack'
+import { viteBundler } from "@vuepress/bundler-vite";
+import { webpackBundler } from "@vuepress/bundler-webpack";
 
 export default defineUserConfig({
-  title: "vuepress-theme-reco",
+  title: "Water Star",
   description: "Just playing around",
   bundler: viteBundler(),
   // bundler: webpackBundler(),
   theme: recoTheme({
     logo: "/logo.png",
-    author: "reco_luan",
-    authorAvatar: "/head.png",
+    author: "Water Star",
+    authorAvatar: "/head.jpg",
     docsRepo: "https://github.com/vuepress-reco/vuepress-theme-reco-next",
     docsBranch: "main",
     docsDir: "example",
@@ -30,71 +30,72 @@ export default defineUserConfig({
       ],
     },
     navbar: [
-      { text: "Home", link: "/" },
-      { text: "Categories", link: "/categories/reco/1.html" },
-      { text: "Tags", link: "/tags/tag1/1.html" },
+      { text: "主页", link: "/" },
+      { text: "分类", link: "/categories/reco/1.html" },
+      { text: "标签", link: "/tags/tag1/1.html" },
       {
-        text: "Docs",
+        text: "文档",
+        // 自定义
         children: [
           { text: "vuepress-reco", link: "/docs/theme-reco/theme" },
           { text: "vuepress-theme-reco", link: "/blogs/other/guide" },
         ],
       },
     ],
-    bulletin: {
-      body: [
-        {
-          type: "text",
-          content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
-          style: "font-size: 12px;",
-        },
-        {
-          type: "hr",
-        },
-        {
-          type: "title",
-          content: "QQ 群",
-        },
-        {
-          type: "text",
-          content: `
-          <ul>
-            <li>QQ群1：1037296104</li>
-            <li>QQ群2：1061561395</li>
-            <li>QQ群3：962687802</li>
-          </ul>`,
-          style: "font-size: 12px;",
-        },
-        {
-          type: "hr",
-        },
-        {
-          type: "title",
-          content: "GitHub",
-        },
-        {
-          type: "text",
-          content: `
-          <ul>
-            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
-            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
-          </ul>`,
-          style: "font-size: 12px;",
-        },
-        {
-          type: "hr",
-        },
-        {
-          type: "buttongroup",
-          children: [
-            {
-              text: "打赏",
-              link: "/docs/others/donate.html",
-            },
-          ],
-        },
-      ],
-    },
+    // bulletin: {
+    //   body: [
+    //     {
+    //       type: "text",
+    //       content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
+    //       style: "font-size: 12px;",
+    //     },
+    //     {
+    //       type: "hr",
+    //     },
+    //     {
+    //       type: "title",
+    //       content: "QQ 群",
+    //     },
+    //     {
+    //       type: "text",
+    //       content: `
+    //       <ul>
+    //         <li>QQ群1：1037296104</li>
+    //         <li>QQ群2：1061561395</li>
+    //         <li>QQ群3：962687802</li>
+    //       </ul>`,
+    //       style: "font-size: 12px;",
+    //     },
+    //     {
+    //       type: "hr",
+    //     },
+    //     {
+    //       type: "title",
+    //       content: "GitHub",
+    //     },
+    //     {
+    //       type: "text",
+    //       content: `
+    //       <ul>
+    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
+    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
+    //       </ul>`,
+    //       style: "font-size: 12px;",
+    //     },
+    //     {
+    //       type: "hr",
+    //     },
+    //     {
+    //       type: "buttongroup",
+    //       children: [
+    //         {
+    //           text: "打赏",
+    //           link: "/docs/others/donate.html",
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // },
     // commentConfig: {
     //   type: 'valine',
     //   // options 与 1.x 的 valineConfig 配置一致
